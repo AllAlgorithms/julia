a513db21e64c1a9c6a6eeb1b73054936bb9f3607
Marc Ittel <marc.ittel@gmail.com>
2021-04-12 14:33:34 +0200

    simplify merge_sort

--- algorithms/sorting/merge_sort.jl
+++ algorithms/sorting/merge_sort.jl
@@ -1,27 +1,28 @@
 
 function mergesort(v)
-    lv = length(v)
-    if lv == 1
+    if length(v) <= 1
         return v
-    else
-        return merge(mergesort(@view v[1:lv÷2]), mergesort(@view v[lv÷2+1:end]))
     end
+    return merge(mergesort(v[1:end÷2]), mergesort(v[end÷2+1:end]))
 end
 
 function merge(v1, v2)
-    if length(v1) == 0
+    if isempty(v1)
         return v2
-    elseif length(v2) == 0
+
+    elseif isempty(v2)
         return v1
+
     elseif v2[1] < v1[1]
-        return vcat(v2[1], merge(v1, @view v2[2:end]))
-    else
-        return vcat(v1[1], merge((@view v1[2:end]), v2))
+        return [v2[1]; merge(v1, v2[2:end])]
+
+    else # v1[1] <= v2[1]
+        return [v1[1]; merge(v1[2:end], v2)]
     end
 end
 
 v = [144, 89, 4, 9, 95, 12, 86, 25]
-println("unsorted array = ",v)
+println("unsorted array = ", v)
 
 sorted_v = mergesort(v)
 
